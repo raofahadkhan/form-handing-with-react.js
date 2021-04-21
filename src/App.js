99df69d6f70eca7bd574ea496ccfbb9fac6a80d8
Rao Fahad <raofahad046@gmail.com>
2021-04-21 12:19:01 -0700

    handled a form with the help of react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-	return <div>Form Handling With React</div>;
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	console.log("Component is rendering again");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(name, email);
+		setName("");
+		setEmail("");
+	};
+
+	const handleChange = (e) => {
+		switch (e.target.name) {
+			case "name":
+				setName(e.target.value);
+				break;
+
+			case "email":
+				setEmail(e.target.value);
+				break;
+
+			default:
+				break;
+		}
+	};
+
+	return (
+		<div>
+			Form Handling With React
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="name">Name : </label>
+				<input
+					type="text"
+					id="name"
+					name="name"
+					placeholder="Enter Your Name"
+					value={name}
+					onChange={handleChange}
+				/>
+				<br />
+				<br />
+
+				<label htmlFor="email">Email : </label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					placeholder="Enter Your Email"
+					value={email}
+					onChange={handleChange}
+				/>
+				<br />
+				<br />
+
+				<input type="submit" value="Submit" />
+			</form>
+		</div>
+	);
 }
 
 export default App;
